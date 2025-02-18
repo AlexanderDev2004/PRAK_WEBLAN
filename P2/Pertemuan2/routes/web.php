ddf3080606a1ffcd330cb9c694bee9954db279c6
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-18 11:11:00 +0700

    feat : menambahkan beberapa controller dan route baru untuk halaman utama, artikel, dan foto

--- P2/Pertemuan2/routes/web.php
+++ P2/Pertemuan2/routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/hello', function ()  {
+Route::get('/hello', function () {
     return "Hello World";
 });
 
@@ -34,25 +39,45 @@ Route::get('/about', function () {
 });
 
 Route::get('/user/{name}', function ($name) {
-    return "Nama saya\n". $name;
+    return "Nama saya\n" . $name;
 });
 
-Route::get('/posts/{post}/comments/{comment}', function
-($postId, $commentId) {
- return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
 Route::get('/articles/{id}', function ($id) {
-    return "Halaman artikel dengan ID ".$id;
+    return "Halaman artikel dengan ID " . $id;
 });
 
 
-Route::get('/user/{name?}', function ($name=null){
-    return "Nama saya\n". $name;
+Route::get('/user/{name?}', function ($name = null) {
+    return "Nama saya\n" . $name;
 });
 
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-    });
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
 
+Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/', [PagesController::class, 'index']);
+// Route::get('/about', [PagesController::class, 'about']);
+
+
+Route::get('/about', AboutController::class);
+Route::get('/articles', ArticleController::class);
+
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
