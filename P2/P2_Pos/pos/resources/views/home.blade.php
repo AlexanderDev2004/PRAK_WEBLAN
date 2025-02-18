f64c03be7cb907f330c62cd3fae1be6f29e2518f
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-18 14:10:17 +0700

    setup_conf : menambahkan struktur dasar aplikasi dengan file konfigurasi, controller, dan tampilan awal

---
+++ P2/P2_Pos/pos/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to POS Alex</h1>
+</body>
+</html>
---
+++ P2/P2_Pos/pos/resources/views/sales/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sales</title>
+</head>
+<body>
+    <h1>Point of Sales</h1>
+</body>
+</html>
