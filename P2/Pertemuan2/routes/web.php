b69250c9fa4ae97b2caf5c7514b2b3d2a35289cd
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-18 09:11:55 +0700

    corn : memperbarui route untuk menampilkan pesan selamat datang dan menambahkan route /about

--- P2/Pertemuan2/routes/web.php
+++ P2/Pertemuan2/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/hello', function ()  {
     return "Hello World";
@@ -23,5 +23,13 @@ Route::get('/hello', function ()  {
 
 Route::get('/world', function () {
     return 'World';
+});
+
+Route::get('/', function () {
+    return "selamat datang";
+});
+
+Route::get('/about', function () {
+    return "NIM : 234172040";
 });
 
